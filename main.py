0e39cbc23581e7adaae03861bd9f6849966e0b88
Gourav Sharma <gouravsharma2806@gmail.com>
2025-04-19 08:52:54 +0530

    added jsonify

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, app
+from flask import Flask, jsonify  # jsonify remove the error
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/",methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def getHome():
-     return "Welcome Home"
+    return "Welcome Home"
 
-@app.route("/books",methods=['GET'])
+
+@app.route("/books", methods=["GET"])
 def getBooks():
-    booklist=["Complete Reference Python","Mastering Python","Deep dive into MySQL"]
-    return booklist
+    booklist = ["Complete Reference Python", "Mastering Python", "Deep dive into MySQL"]
+    return jsonify(booklist)  # added jsonify
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(debug=True)
