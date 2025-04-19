ea7d5e580897da2d1f685d943bcc718ca0d85e2c
Gourav Sharma <gouravsharma2806@gmail.com>
2025-04-19 09:32:37 +0530

    created new branch and home page

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from flask import Flask, jsonify  # jsonify remove the error
+from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder="template")  # using custom folder
+
+books = [
+    {"id": 1, "title": "Mastering Python", "cost": 600},
+    {"id": 2, "title": "Inside MySQL", "cost": 500},
+    {"id": 3, "title": "Mastering Java", "cost": 800},
+    {"id": 4, "title": "Machine Learning in details", "cost": 900},
+]
 
 
 @app.route("/", methods=["GET"])
 def getHome():
-    return "Welcome Home"
+    print("Welcome Home")
+    return render_template("home.html")
 
 
 @app.route("/books", methods=["GET"])
 def getBooks():
-    booklist = ["Complete Reference Python", "Mastering Python", "Deep dive into MySQL"]
-    return jsonify(booklist)  # added jsonify
+    return jsonify(books)
 
 
 if __name__ == "__main__":
